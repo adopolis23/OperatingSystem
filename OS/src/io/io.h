39cd689c74d157039dab5ee41d5b0ed1ec6128fa
adopolis23 <weinhofer@usf.edu>
2025-09-03 20:02:20 -0400

    IO updates

--- OS/src/io/io.h
+++ OS/src/io/io.h
@@ -14,44 +14,46 @@
 
     void outb(unsigned short port, unsigned char data);
 
-    void fb_move_cursor(unsigned short pos)
+    void fb_move_cursor(unsigned short pos);
+
+    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
+
+    void print_message_beginning_fb(const char* message, unsigned char fg, unsigned char bg);
+
+    void clear_screen_after_position(unsigned int pos);
+
+    //configure the serial ports
+    /* All the I/O ports are calculated relative to the data port. This is because
+    * all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
+    * order, but they start at different values.
+    */
+    #define SERIAL_COM1_BASE 0x3F8 // COM1 base port 
+    #define SERIAL_DATA_PORT(base) (base)
+    #define SERIAL_FIFO_COMMAND_PORT(base) (base + 2)
+    #define SERIAL_LINE_COMMAND_PORT(base) (base + 3)
+    #define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+    #define SERIAL_LINE_STATUS_PORT(base) (base + 5)
+
+    /* SERIAL_LINE_EXPECT_HIGHEST_FIRST:
+    * Tells the serial port to expect first the highest 8 bits on the data port,
+    * then the lowest 8 bits will follow
+    */
+    #define SERIAL_LINE_EXPECT_HIGHEST_FIRST 0x80
+
+    /** serial_configure_baud_rate:
+    * Sets the speed of the data being sent. The default speed of a serial
+    * port is 115200 bits/s. The argument is a divisor of that number, hence
+    * the resulting speed becomes (115200 / divisor) bits/s.
+    *
+    * @param com The COM port to configure
+    * @param divisor The divisor
+    */
+
+    void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
     {
-        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-        outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
-        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-        outb(FB_DATA_PORT, pos & 0x00FF);
-    }
-
-    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-    {
-        // write text to framebuffer
-        //fb now points to beginning address of frame buffer
-        char* fb = (char*) 0x000B8000;
-
-        fb[i] = c;
-        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-    }
-
-    void print_message_beginning_fb(const char* message, unsigned char fg, unsigned char bg)
-    {
-        unsigned int i = 0;
-        while (message[i] != '\0')
-        {
-            fb_write_cell(i*2, message[i], fg, bg);
-            i++;
-        }
-
-        fb_move_cursor(i);
-    }
-
-    void clear_screen_after_position(unsigned int pos)
-    {
-        unsigned int i = pos;
-        while (i < 2000)
-        {
-            fb_write_cell(i*2, ' ', 0, 0);
-            i++;
-        }
+        outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_EXPECT_HIGHEST_FIRST);
+        outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
+        outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
     }
 
 #endif
