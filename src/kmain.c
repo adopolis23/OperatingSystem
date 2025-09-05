f922df8e86f12fa644f99ad7a040e89d9cca9cea
adopolis23 <weinhofer@usf.edu>
2025-09-04 22:17:27 -0400

    Restructured Repository

--- src/kmain.c
+++ src/kmain.c
@@ -4,11 +4,12 @@
 
 void kmain(void)
 {
+    char buf[11];
+
     //log to serial port com1 - does not decrement sp
     serial_write_string(SERIAL_COM1_BASE, "Entry into kernel kmain successful\n");
 
     unsigned int sp = get_sp();
-    char buf[11];
     itoa_hex(sp, buf);
     serial_write_string(SERIAL_COM1_BASE, "ESP = ");
     serial_write_string(SERIAL_COM1_BASE, buf);
@@ -20,4 +21,10 @@ void kmain(void)
     print_message_beginning_fb(message, 0, 2);
     clear_screen_after_position(12);
 
+    //_end is defined in the linker script as the end of the kernel after the .bss section 
+    extern char _end;
+    unsigned int kernel_end = (unsigned int) &_end;
+    itoa_hex(kernel_end, buf);
+    serial_write_string(SERIAL_COM1_BASE, "Kernel end at: ");
+    serial_write_string(SERIAL_COM1_BASE, buf);
 }
